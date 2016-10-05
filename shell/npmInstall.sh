43a374dbb2f395a2953106a49f88e28bd5bcbde3
Louis de Charson <ltdc2121@hotmail.fr>
2016-10-05 08:46:56 +0200

    minor changes shell

--- shell/npmInstall.sh
+++ shell/npmInstall.sh
@@ -2,11 +2,11 @@
 
 echo "mise à jour des fichiers (1/2)"
 cd fetchers/
-npm install
+sudo npm install
 cd ../web
-npm install
+sudo npm install
 cd ../api
-npm install
+sudo npm install
 echo "Fin de la mise à jour des packages"
 
 
@@ -14,18 +14,18 @@ cd ../web
 sudo pm2 describe server.js > /dev/null
 RUNNING=$?
 if [ "${RUNNING}" -ne 0 ]; then
-  sudo pm2 start server.js
+  pm2 start server.js
 else
-  sudo pm2 restart server.js 
+  pm2 restart server.js 
 fi;
 
 cd ../api
 sudo pm2 describe server.js > /dev/null
 RUNNING=$?
 if [ "${RUNNING}" -ne 0 ]; then
-  sudo pm2 start server.js
+  pm2 start server.js
 else
-  sudo pm2 restart server.js 
+  pm2 restart server.js 
 fi;
 
 echo "Scripts relancés"
