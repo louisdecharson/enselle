ffd35de88fb2dffe0d369486c5ce46bef8e2bf69
Louis de Charson <ltdc2121@hotmail.fr>
2017-04-11 15:06:22 +0200

    package

--- shell/npmInstall.sh
+++ shell/npmInstall.sh
@@ -3,27 +3,30 @@
 echo "mise à jour des fichiers (1/2)"
 cd ../fetchers/
 sudo npm install
-cd ../web
+cd ../web/
+echo ${PWD}
 sudo npm install
-cd ../api
+cd ../api/
 sudo npm install
 echo "Fin de la mise à jour des packages"
 
-
-cd ../web
+echo "Lancement des scripts (2/2)"
+cd ../web/
+echo ${PWD}
 sudo pm2 describe server > /dev/null
 RUNNING=$?
 if [ "${RUNNING}" -ne 0 ]; then
-  pm2 start server.js
+  pm2 start server.js -n web
 else
   pm2 restart server.js 
 fi;
 
-cd ../api
+cd ../api/
+echo ${PWD}
 sudo pm2 describe server > /dev/null
 RUNNING=$?
 if [ "${RUNNING}" -ne 0 ]; then
-  pm2 start server.js
+  pm2 start server.js -n api
 else
   pm2 restart server.js 
 fi;
